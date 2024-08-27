8b3ffa66c78206c2b6aadd826dd3d4a2374de699
softmanu-tech <walelae86@gmail.com>
2024-08-27 04:35:13 -0700

    advance

--- quote-request-backend/src/server.ts
+++ quote-request-backend/src/server.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app: Express = express();
 const PORT: number = parseInt(process.env.PORT || '5000');
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     optionsSuccessStatus: 200
   };
   
